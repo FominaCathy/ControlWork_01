2510ff739ef06fe4abbda4c10d3fdc616201d688
FominaCathy <99419802+FominaCathy@users.noreply.github.com>
2022-05-08 14:24:31 +0400

    добавлен метод вывода массива на экран

--- Program.cs
+++ Program.cs
@@ -4,11 +4,22 @@
 
 // подсчет кол-ва элементов в заданном массиве длина которых меньше либо равна 3 символа
 // заполнение нового массива -основная часть. блок-схема на нее
-// вывод на экран
+// вывод на экран 
 
-string[] arrayIn = new string[];
-string[] arrayOut = new string[];
+
+string[] arrayIn = {"Rom", "London", "Dom", "hole", "caverne"};//new string[];
+//string[] arrayOut = new string[];
 int i = 0;
 int j = 0;
 
+PrintArray(arrayIn);
 
+
+void PrintArray (string[] arr) // вывод массива на экран
+{
+    int i = 0;
+    int size = arr.Length;
+    for (i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+
+}
